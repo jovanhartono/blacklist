ea37a44afa07c9904e74a3d24036832c06a55699
jovan <jovanus@yahoo.co.id>
2021-09-26 16:58:03 +0700

    Adding @Repository annotation.

--- src/main/java/project/blacklist/repository/SuspectPaymentDetailRepository.java
+++ src/main/java/project/blacklist/repository/SuspectPaymentDetailRepository.java
@@ -1,7 +1,9 @@
 package project.blacklist.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import project.blacklist.model.SuspectPaymentDetail;
 
+@Repository
 public interface SuspectPaymentDetailRepository extends JpaRepository<SuspectPaymentDetail, Long> {
 }
--- src/main/java/project/blacklist/repository/SuspectRepository.java
+++ src/main/java/project/blacklist/repository/SuspectRepository.java
@@ -1,6 +1,8 @@
 package project.blacklist.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface SuspectRepository extends JpaRepository<SuspectRepository, Long> {
 }
